8f5b8fabbc17248a1fd4b4cab57acbcb5bbbfb2d
denisPios <104821538+denisPios@users.noreply.github.com>
2023-12-15 15:52:30 +0100

    upd cleaned version
    Need to add numpy in requires
    now also stores last data read in self.data for future plots.

--- interface/app.py
+++ interface/app.py
@@ -8,6 +8,9 @@ import bleak
 from bleak import BleakScanner
 from bleak import BleakClient
 import asyncio
+import numpy as np
+
+
 
 
 class unatest(toga.App):
@@ -15,59 +18,77 @@ class unatest(toga.App):
     def startup(self):
         main_box = toga.Box(style=Pack(direction=COLUMN))
 
-        
-        
-        button = toga.Button(
-            "Say Hello!",
-            on_press=self.say_hello,
-            style=Pack(padding=5)
-        )
         button_disconnect = toga.Button("Disconnect", on_press=self.disconnectBLE,style=Pack(padding=5))
+        button_connect = toga.Button("Start Connection", on_press=self.connectBLE,style=Pack(padding=5))
 
-       
-        main_box.add(button)
+        self.label_status = toga.Label("Device: None")
+        self.label_connection = toga.Label("Currently Disconnected")
+        main_box.add(self.label_connection)
+        main_box.add(self.label_status)
         main_box.add(button_disconnect)
+        main_box.add(button_connect)
         
         self.show_label = toga.Label("Values:")
         main_box.add(self.show_label)
 
+
+
+
+
         self.main_window = toga.MainWindow(title=self.formal_name)
         self.main_window.content = main_box
         self.main_window.show()
-        self.connected = True
-    
-    
-    def disconnectBLE(self,widget):
-        self.connected= False
-        #widget.text = "something"
+        self.connected = False
+        self.data = np.zeros((3,10))
+        self.device = None
+      
+        
+
+    async def connectBLE(self,widget):
+            if not self.connected :
+                widget.text = "Start Connection"
+                await self.scanBLE() #founds the addres to connect
+                
+                async with bleak.BleakClient(self.device.address) as client:
+                        self.connected = True
+                        widget.text="Currently Connected"
+                        self.label_connection.text ="Currently Connected"
+                        await client.start_notify("00002A56-0000-1000-8000-00805f9b34fb", self.handle_notification)
+                        while self.connected: # currently hears what arduino is sending
+                            await asyncio.sleep(1) #could change frequency of readings 
+                        await client.stop_notify("00002A56-0000-1000-8000-00805f9b34fb")
+                        widget.text ="Re Connect"
+            print(self.data) #just to get idea how the values are stored
 
 
-    async def say_hello(self, widget):
-        widget.text = "scanning"
-        device_found = None
+    
+    def disconnectBLE(self,widget): #jto stop the connection
+        self.connected = False
+        self.label_connection.text ="Currently Disconnected"
+       
+        
+
+    async def scanBLE(self): #to find the addres of the arduino
+        self.label_status.text ="Scanning"
         scanned = await bleak.BleakScanner.discover()
         for device in scanned:
             if device.name == "Arduino Datanauts":
-                widget.text = device.name
-                device_found = device
+                self.device = device
+        self.label_status.text = "Device : " + self.device.name
 
-        #self.connected = True
-        print(self.connected)
-        async with bleak.BleakClient(device_found.address) as client:
-            #self.connected=True
-            await client.start_notify("00002A56-0000-1000-8000-00805f9b34fb", self.handle_notification)
-            while self.connected:
-                widget.text = "connected"
-            await client.stop_notify("00002A56-0000-1000-8000-00805f9b34fb")
-        widget.text = "disconnected"
-        self.show_label.text += "read values"
+        
 
 
 
     def handle_notification(self, sender:str, data:bytearray): 
-        data_read = data.decode()
-        name_file = "appa_data"+".csv"
+        data_read = data.decode()           # incoming data as a string
+        data = np.fromstring(data_read, dtype=float, sep=',') #incoming data as an array
+        for i in range(3): #update 3 for the 6 after we send more data, need to change the dimension of self.data also
+            self.data[i]=np.roll(self.data[i],-1)#shifts array one
+            self.data[i][-1]= data[i]   #update last value
+
         self.show_label.text = data_read
+
 
 def main():
     return unatest()
