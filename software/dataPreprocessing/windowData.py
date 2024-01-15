9583e058e5cc94996f20ea8f857f030bd925560b
HazimDaoud <daoud.hazem98@gmail.com>
2024-01-15 21:01:13 +0100

    Use the last element instead of the majority

--- software/dataPreprocessing/windowData.py
+++ software/dataPreprocessing/windowData.py
@@ -7,9 +7,10 @@ def createSlidingWindows(data, labels, windowSize):
     for i in range(len(data) - windowSize + 1):
         window = data[i : i + windowSize]
         labelCounts = Counter(labels[i : i + windowSize])
-        majority = labelCounts.most_common(1)[0][0]
-        windows.append(window)
-        correspondingLabels.append(majority)
+        # majority = labelCounts.most_common(1)[0][0]
+        # windows.append(window)
+        label = labels[i + windowSize -1]
+        correspondingLabels.append(label)
         
     windows = np.array(windows)
     correspondingLabels = np.array(correspondingLabels)
